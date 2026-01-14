fb24c6659a31aab49cd8c5c9740016a7be84d8ce
kapibara001 <trostikshop@gmail.com>
2026-01-14 23:05:53 +0500

    vacancy parts

--- resources/views/layouts/vacancies.blade.php
+++ resources/views/layouts/vacancies.blade.php
@@ -1,7 +1,18 @@
 <div class="vacancies-container">
     <div class="vacancies-content">
         <div class="vacancy">
-            
+            <div class="vacancy-name">
+
+            </div>
+            <div class="vacancy-salary">
+                {{-- salary from-to  --}}
+            </div>
+            <div class="vacancy-city">
+
+            </div>
+            <div class="">
+
+            </div>
         </div>
     </div>
 </div>
