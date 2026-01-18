8d0229e0fb648535fc69957d7fd8f4364f1526a5
kapibara001 <trostikshop@gmail.com>
2026-01-18 15:31:21 +0500

    login dev

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use illuminate\Http\Request;
+
 class LoginController extends Controller {
     public function login(Request $request) {
-        # Handle login logic here
+        $email = $request->input('email');
+        $password = hash('sha256', $request->input('password'));
+
+        return redirect('/');
     }
 
     public function openLoginWin() {
